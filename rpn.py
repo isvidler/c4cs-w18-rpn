72a3988dc0668a81350e143fc420f3d4cac256bb
isvidler <isvidler@umich.edu>
2018-03-07 22:26:47 -0500

    Final versions of files from class plus .travis.yml

--- rpn.py
+++ rpn.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python3
 
-def calculate(arg):
+import operator
+
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
+def calculate(myarg):
     stack = list()
-    for token in arg.split():
+    for token in myarg.split():
         try:
-            value = int(token)
-            stack.append(value)
+            token = int(token)
+            stack.append(token)
         except ValueError:
-            arg1 = stack.pop()
+            function = operators[token]
             arg2 = stack.pop()
-            return arg1 + arg2
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
 
 def main():
     while True:
-        print(calculate(input('rpn calc> ')))
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
